db08ec99e90bd5634f62b935753838646861e57c
Mark Hughes <m.hughes@henimac060.home>
2022-04-06 18:38:35 +0100

    added ;

--- Type Conversion/app.js
+++ Type Conversion/app.js
@@ -7,45 +7,45 @@ val = String(5);
 val = String(4+4);
 // Output
 console.log(val)
-console.log(typeof val)
-console.log(val.length)
+console.log(typeof val);
+console.log(val.length);
 
 // Boolean to string
 val = String(true);
 // Output
 console.log(val)
-console.log(typeof val)
-console.log(val.length)
+console.log(typeof val);
+console.log(val.length);
 
 // Date to string
 val = String(new Date());
 // Output
 console.log(val)
-console.log(typeof val)
-console.log(val.length)
+console.log(typeof val);
+console.log(val.length);
 
 // Array to string
 val = String([1,2,3,4]);
 // Output
-console.log(val)
-console.log(typeof val)
-console.log(val.length)
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
 
 // toString
-val = (5).toString()
-val = (true).toString()
+val = (5).toString();
+val = (true).toString();
 // Output
-console.log(val)
-console.log(typeof val)
-console.log(val.length)
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
 
 // String to number
 val = Number('5');
 // Output
-console.log(val)
-console.log(typeof val)
-//console.log(val.length)
-console.log(val.toFixed(3))
+console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+console.log(val.toFixed(3));
 
 // Boolean to number
 val = Number(true);
@@ -53,13 +53,13 @@ val = Number(null);
 val = Number(null); // Nan Not a number
 val = Number([1,2,3]); // Nan Not a number
 val = Number(false);
-val = parseInt(100.30)
-val = parseFloat(100.3)
+val = parseInt(100.30);
+val = parseFloat(100.3);
 // Output
-console.log(val)
-console.log(typeof val)
-//console.log(val.length)
-console.log(val.toFixed(2)) // to 2 decial places
+console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+console.log(val.toFixed(2)); // to 2 decial places
 
 
 // TYPE COHERSION
@@ -68,8 +68,8 @@ const val1 = String(1);
 const val2 = 2;
 const sum = val1 + val2;
 // Output
-console.log(sum)
-console.log(typeof sum)
+console.log(sum);
+console.log(typeof sum);
 
 
 // String to Number
@@ -77,5 +77,5 @@ const val3 = String(1);
 const val4 = 2;
 const sum2 = Number(val1 + val2);
 // Output
-console.log(sum2)
-console.log(typeof sum2)
+console.log(sum2);
+console.log(typeof sum2);
