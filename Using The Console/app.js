6d8979a9ded5b581b0b3b2db4ea1512036164da6
Mark Hughes <m.hughes@henimac060.home>
2022-04-06 16:43:28 +0100

    Lesson 1

---
+++ Using The Console/app.js
@@ -0,0 +1,13 @@
+// Log to the console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2}); //object literal
+console.table({a:1, b:2});
+console.error('This is some error');
+console.warn('This is a warning');
+console.time('Hello')
+  console.timeEnd('Hello')
