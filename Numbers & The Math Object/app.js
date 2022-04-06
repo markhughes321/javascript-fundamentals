57d2ea80b314dd62f69b4965f0cf1ee89c8d4c6c
Mark Hughes <m.hughes@henimac060.home>
2022-04-06 19:01:58 +0100

    Numbers & The Math Object

---
+++ Numbers & The Math Object/app.js
@@ -0,0 +1,55 @@
+// Numbers & The Math Object
+
+// Simple math with numbers
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val1 = num1 + num2;
+console.log(val1)
+
+val2 = num1 * num2;
+console.log(val2)
+
+val3 = num1 - num2;
+console.log(val3)
+
+val4 = num1 / num2;
+console.log(val4)
+
+val5 = num2 % num1; // remaining
+console.log(val5)
+
+// Math Object
+val6 = Math.PI;
+console.log(val6)
+
+val7 = Math.round(2.8);
+console.log(val7)
+
+val8 = Math.ceil(2.4);
+console.log(val8)
+
+val9 = Math.floor(2.8);
+console.log(val9)
+
+val10 = Math.sqrt(64);
+console.log(val10)
+
+val11 = Math.abs(-3);
+console.log(val11)
+
+val12 = Math.pow(8, 2);
+console.log(val12)
+
+val13 = Math.min(8, 2, 11, 30, 44);
+console.log(val13)
+
+val14 = Math.max(8, 2, 11, 30, 44);
+console.log(val14)
+
+val15 = Math.random();
+console.log(val15)
+
+val16 = Math.floor(Math.random() * 20 + 1);  // random number between 1-20
+console.log(val16)
